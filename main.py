434265d74ddc3131ce2b923199f67584a708a7e1
hemangsengar <hemang.sengar@gmail.com>
2024-09-02 19:18:30 +0530

    Added GUI

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+# Create a canvas instance
+myturtle = turtle.Turtle()
+
+
+myturtle.penup()
+
+myturtle.goto(10,10)
+
+myturtle.pendown()
+myturtle.forward(100) #move 100 pixel
+myturtle.left(90)# move 90 degree
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+
+
+turtle.done()
